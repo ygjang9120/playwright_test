f568da65e6c6e79f39ed929e1a2cb18d968b42e7
jang_9120 <ygjang9120@gmail.com>
2025-08-08 14:24:51 +0900

    delete schedule in e2e-test

--- tests/adeka.spec.ts
+++ tests/adeka.spec.ts
@@ -248,6 +248,41 @@ test.describe('전체 LOT 대상 COA 다운로드 및 상세 검증', () => {
     await runProductValidation(browser, 'HPL-02', 'hpl-02', ['F_AL', 'F_CR', 'F_CA'],1);
   });
 
+  test('ACP-2 제품의 모든 LOT 검증', async ({ browser }) => {
+    test.setTimeout(1800_000);
+  await runProductValidation(browser, 'ACP-2', 'acp-2', ['F_AL','F_CA','F_CR'], 1);
+  });
+
+  test('ACP-3 제품의 모든 LOT 검증', async ({ browser }) => {
+    test.setTimeout(1800_000);
+    await runProductValidation(browser, 'ACP-3', 'acp-3', ['F_AL','F_CA','F_CR'], 1);
+  });
+
+  test('TMA-F 제품의 모든 LOT 검증', async ({ browser }) => {
+    test.setTimeout(1800_000);
+    await runProductValidation(browser, 'TMA-F', 'tma-f', ['F_AL','F_CA','F_CR'], 1);
+  });
+
+  test('SFA-1 제품의 모든 LOT 검증', async ({ browser }) => {
+    test.setTimeout(1800_000);
+    await runProductValidation(browser, 'SFA-1', 'sfa-1', ['F_AL','F_CA','F_CR'], 1);
+  });
+
+  test('NCE-2 제품의 모든 LOT 검증', async ({ browser }) => {
+    test.setTimeout(1800_000);
+    await runProductValidation(browser, 'NCE-2', 'nce-2', ['F_AL','F_CA','F_CR'], 1);
+  });
+
+  test('GMP-02 제품의 모든 LOT 검증', async ({ browser }) => {
+    test.setTimeout(1800_000);
+    await runProductValidation(browser, 'GMP-02', 'cmp-02', ['F_AL','F_CA','F_CR'], 1);
+  });
+
+  test('ECH 제품의 모든 LOT 검증', async ({ browser }) => {
+    test.setTimeout(1800_000);
+    await runProductValidation(browser, 'ECH', 'ech', ['F_AL','F_CA','F_CR'], 1);
+  });
+
 
   // 3. 모든 테스트가 끝난 후, 최종 결과 처리
   test.afterAll(async () => {
